aa5a17251cb5064d7e3b3539e4959fe104f80b5c
aelious <aeliousx@gmail.com>
2024-09-24 10:39:54 -0500

    Upload P191

---
+++ Assignments/LeetCode Problems/P191/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        string binary = "";
+        int count = 0;
+        while (n != 0) {
+            if(n%2 == 0) {
+                binary = "0" + binary;
+            } else {
+                binary = "1" + binary;
+            }
+            n /= 2;
+        }
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary[i] == '1'){
+                count++;
+            }
+        }
+        return count;
+    }
+};
